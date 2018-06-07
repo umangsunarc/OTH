0902cebdb08fb2e392d059d82135bb1dd3bd7d96
umangsunarc <umang.patwa@sunarctechnologies.com>
2018-06-07 18:16:15 +0530

    Completed first phase withot real graph data

--- Wollo.API/Config.cs
+++ Wollo.API/Config.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,8 +23,69 @@ namespace Wollo.API
     }
     public class SymbolsTypes
     {
+        public string name { get; set; }   
         public string value { get; set; }
-        public string name { get; set; }        
+            
+    }
+    public class SymbolAPI
+    {
+        public string description { get; set; }
+        public bool has_intraday { get; set; }
+        public bool has_no_volume { get; set; }
+        public int minmov { get; set; }
+        public int minmov2 { get; set; }
+        public string name { get; set; }
+        public int pointvalue { get; set; }
+        public int pricescale { get; set; }
+        public string session { get; set; }
+        public string[] supported_resolutions { get; set; }
+        public string ticker { get; set; }
+        public string timezone { get; set; }
+        public string type { get; set; }
+    }
+    public class SymbolGroupAPI
+    {
+        public string[] symbol { get; set; }
+        public string[] description { get; set; }
+
+        [JsonProperty(PropertyName = "exchange-listed")]
+        public string exchangeListed {get; set;}
+
+        [JsonProperty(PropertyName = "exchange-traded")]
+        public string exchangeTraded { get; set; }
+        public int minmovement { get; set; }
+        public int minmovement2 { get; set; }
+        public int[] pricescale { get; set; }
+
+
+        [JsonProperty(PropertyName = "has-dwm")]
+        public bool hasDwm { get; set; }
+
+        [JsonProperty(PropertyName = "has-intraday")]
+        public bool hasIntraday { get; set; }
+
+        [JsonProperty(PropertyName = "has-no-volume")]
+        public bool[] hasNoVolume { get; set; }
+        public string[] type { get; set; }
+        public string[] ticker { get; set; }
+        public string timezone { get; set; }
+
+        [JsonProperty(PropertyName = "session-regular")]
+        public string sessionRegular { get; set; }
+               
+       
+       
+    }
+
+    public class HistoryAPI
+    {
+        public double[] c { get; set; }
+        public double[] h { get; set; }
+        public double[] l { get; set; }
+        public double[] o { get; set; } 
+        public string s { get; set; }
+        public long[] t { get; set; }
+        public long[] v { get; set; }
     }
    
 }
--- Wollo.API/Controllers/QueueController.cs
+++ Wollo.API/Controllers/QueueController.cs
@@ -432,9 +432,268 @@ namespace Wollo.API.Controllers
             obj.supports_search = true;
             obj.supports_marks = false;
             obj.supportedResolutions = new string[] { "1", "15", "30", "60", "D", "2D", "3D", "W", "3W", "M", "6M" };
-            obj.exchanges = new Exchanges[] { new Exchanges { name = "NSC", value = "NSC", desc = "NSC" } };
-            obj.symbolsTypes = new SymbolsTypes[] { new SymbolsTypes { name = "Stock", value = "Stock" } };          
+            obj.exchanges = new Exchanges[] { new Exchanges { name = "BITFINEX", value = "BITFINEX", desc = "BITFINEX" } };
+            obj.symbolsTypes = new SymbolsTypes[] { new SymbolsTypes { name = "Crypto", value = "crypto" } };          
             response = request.CreateResponse<Config>(HttpStatusCode.OK, obj);
+            return response;
+        }
+        //[HttpGet]
+        //public HttpResponseMessage symbol_info(HttpRequestMessage request, string group = "BITFINEX")
+        //{
+
+        //    HttpResponseMessage response = null;
+        //    SymbolGroupAPI obj = new SymbolGroupAPI();
+           
+        //            obj.symbol = new string[] {"BTCUSD", "ETHUSD"};
+        //            obj.description = new string[] {"Bitcoin / Dollar","Ethereum / Dollar"};
+        //            obj.exchangeListed = "BITFINEX";
+        //            obj.exchangeTraded = "BITFINEX";
+        //            obj.minmovement = 1;
+        //            obj.minmovement2 = 0;
+        //            obj.pricescale = new int[] {10,100};
+        //            obj.hasDwm = true;
+        //            obj.hasIntraday = true;
+        //            obj.hasNoVolume = new bool[] { false, false };
+        //            obj.type = new string[] {"crypto","crypto"} ;
+        //            obj.ticker = new string[] { "BTCUSD", "ETHUSD" };
+        //            obj.timezone = "Asia/Hong_Kong";
+        //            obj.sessionRegular = "0900-1600";
+               
+        //    response = request.CreateResponse<SymbolGroupAPI>(HttpStatusCode.OK, obj);
+        //    return response;        
+        //}
+
+        [HttpGet]
+        public HttpResponseMessage symbols(HttpRequestMessage request, string symbol = "BTCUSD")
+        {
+
+            HttpResponseMessage response = null;
+            SymbolAPI obj = new SymbolAPI();
+
+            obj.name = "BTCUSD";
+            obj.description = "Bitcoin / Dollar";
+            obj.has_intraday = false;
+            obj.has_no_volume = false;
+            obj.minmov = 1;
+            obj.minmov2 = 0;
+            obj.pricescale = 100;
+            obj.pointvalue = 1;
+            obj.supported_resolutions = new string[] { "D", "2D", "3D", "W", "3W", "M", "6M" };
+            obj.type = "crypto";
+            obj.ticker = "BTCUSD";
+            obj.timezone = "Asia/Hong_Kong";
+            obj.session = "0900-1600";
+
+            response = request.CreateResponse<SymbolAPI>(HttpStatusCode.OK, obj);
+            return response;
+        }
+
+
+        [HttpGet]
+        public HttpResponseMessage history(HttpRequestMessage request, string symbol, string resolution, long from, long to)
+        {
+            int stockId = 1;
+            QueueData queueData = new QueueData();
+            QueueDataViewModel model = new QueueDataViewModel();
+            List<Traded_History_Master> tradedHistoryResult = new List<Traded_History_Master>();
+            List<Transaction_History> result = new List<Transaction_History>();
+            result = AutoMapperHelper.GetInstance().Map<List<Transaction_History>>(_transactionRepository.FindBy(x => x.stock_code_id == stockId && x.status_id != 3).ToList().OrderByDescending(y => y.updated_date));
+            tradedHistoryResult = AutoMapperHelper.GetInstance().Map<List<Traded_History_Master>>(_tradedHistoryMasterRepository.FindBy(x => x.stock == stockId).ToList());
+            List<Stock_Code> stock_code = AutoMapperHelper.GetInstance().Map<List<Stock_Code>>(_stockCodeRepository.GetAll().ToList());
+            Units_Master unitMasterData = AutoMapperHelper.GetInstance().Map<Units_Master>(_unitMasterRepository.GetAll().Where(x => x.stock_id == stockId).FirstOrDefault());
+            if (unitMasterData == null)
+            {
+                unitMasterData = new Units_Master();
+                unitMasterData.minimum_lot = 0;
+                unitMasterData.minimum_rate = 0;
+                unitMasterData.points_equivalent = 1;
+                unitMasterData.unit = "Lot";
+                unitMasterData.updated_date = DateTime.UtcNow;
+                unitMasterData.created_date = DateTime.UtcNow;
+                unitMasterData.created_by = "System";
+                unitMasterData.updated_by = "System";
+            }
+            HttpResponseMessage response = null;
+            DateTime today = DateTime.Today.Date;
+            DateTime? lastTradedTime = new DateTime();
+            DateTime previousYearDate = DateTime.Today.AddYears(-1).Date;
+            var yesterday = DateTime.Today.AddDays(-1);
+            if (result.Where(j => j.status_id == 1 && j.queue_action == "bid").Any())
+            {
+                queueData.highestBidRate = result.Where(j => j.status_id == 1 && j.queue_action == "bid").Max(i => i.rate);
+            }
+            else
+            {
+                queueData.highestBidRate = 0.0F;
+            }
+            if (result.Where(j => j.status_id == 1 && j.queue_action == "ask").Any())
+            {
+                queueData.LowestAskRate = result.Where(j => j.status_id == 1 && j.queue_action == "ask").Min(i => i.rate);
+            }
+            else
+            {
+                queueData.LowestAskRate = 0.0F;
+            }
+            if (tradedHistoryResult.Count > 0)
+            {
+                if (tradedHistoryResult.Where(x => x.created_date >= previousYearDate).Any())
+                {
+                    float previousYearMinRate = tradedHistoryResult.Where(x => x.created_date >= previousYearDate).Select(y => y.bid_price).Min();
+                    float previousYearMaxRate = tradedHistoryResult.Where(x => x.created_date >= previousYearDate).Select(y => y.bid_price).Max();
+                    queueData.oneYearMinMaxRate = previousYearMinRate.ToString("0.00") + "-" + previousYearMaxRate.ToString("0.00");
+                }
+                else
+                {
+                    queueData.oneYearMinMaxRate = 0.0F + "-" + 0.0F;
+                }
+                if (tradedHistoryResult.Where(x => x.created_date >= yesterday && x.created_date < today).Any())
+                {
+                    queueData.prevClose = tradedHistoryResult.Where(x => x.created_date >= yesterday).Select(y => y.bid_price).LastOrDefault();
+                }
+                else
+                {
+                    queueData.prevClose = tradedHistoryResult.Select(y => y.bid_price).LastOrDefault();
+                }
+                if (tradedHistoryResult.Where(x => x.created_date > today).Any())
+                {
+                    lastTradedTime = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.updated_date).LastOrDefault();
+                    queueData.dayHigh = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.bid_price).Max(); //Maximum traded bid price of current date
+                    queueData.dayLow = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.bid_price).Min();  //Minimum traded bid price of current date
+                    queueData.open = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.bid_price).FirstOrDefault(); //Bid price at which first trading happened on current date
+                    queueData.noOfTraded = tradedHistoryResult.Where(x => x.created_date > today).Count(); //Total number of trading happened on today date
+                    queueData.lastTradedPrice = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.bid_price).LastOrDefault();
+                    //string datePart = String.Format("{0:dddd, MMMM d, yyyy HH:mm:ss}", lastTradedTime);
+                    //queueData.lastTradedTime = String.Format("{0:F}", lastTradedTime) + " GMT +8";
+                    queueData.lastTradedTime = String.Format("{0:dddd, MMMM d, yyyy HH:mm:ss}", lastTradedTime) + " GMT +8";
+                    queueData.totatlVolume = tradedHistoryResult.Where(x => x.created_date > today).Select(y => y.amount).Sum(); //Total volume traded on current date
+                    if (tradedHistoryResult.Where(x => x.created_date > today).Count() >= 2)
+                    {
+                        var secondLastTradedPrice = tradedHistoryResult.Where(x => x.created_date > today).Reverse().Skip(1).Take(1).Select(y => y.bid_price).LastOrDefault();
+                        queueData.lastTradedPriceDifference = secondLastTradedPrice - queueData.lastTradedPrice;
+                        queueData.lastTradedPricePercentDifference = ((secondLastTradedPrice - queueData.lastTradedPrice) / queueData.lastTradedPrice) * 100;
+                    }
+                    else
+                    {
+                        queueData.lastTradedPriceDifference = queueData.lastTradedPrice;
+                        queueData.lastTradedPricePercentDifference = 0;
+                    }
+                    List<Traded_History_Master> data = tradedHistoryResult.Where(x => x.created_date > today).ToList();
+                    queueData.valueTraded = 0.0F;
+                    foreach (var item in data)
+                    {
+                        queueData.valueTraded += item.bid_price * item.amount;
+                    }
+                }
+                else
+                {
+                    lastTradedTime = tradedHistoryResult.Select(y => y.updated_date).LastOrDefault();
+                    queueData.lastTradedPrice = tradedHistoryResult.Select(y => y.bid_price).LastOrDefault(); //Last traded bid price of current date
+                    //string datePart = String.Format("{0:dddd, MMMM d, yyyy HH:mm:ss}", lastTradedTime);
+                    //queueData.lastTradedTime = String.Format("{0:F}", lastTradedTime) + " GMT +8";
+                    queueData.lastTradedTime = String.Format("{0:dddd, MMMM d, yyyy HH:mm:ss}", lastTradedTime) + " GMT +8";
+                    if (tradedHistoryResult.Count() >= 2)
+                    {
+                        var secondLastTradedPrice = tradedHistoryResult.Where(x => x.created_date != null).Reverse().Skip(1).Take(1).Select(y => y.bid_price).LastOrDefault();
+                        queueData.lastTradedPriceDifference = secondLastTradedPrice - queueData.lastTradedPrice;
+                        queueData.lastTradedPricePercentDifference = ((secondLastTradedPrice - queueData.lastTradedPrice) / queueData.lastTradedPrice) * 100;
+                    }
+                    else
+                    {
+                        queueData.lastTradedPriceDifference = 0.0F;
+                        queueData.lastTradedPricePercentDifference = 0.0f;
+                    }
+                    queueData.dayHigh = 0.0F;
+                    queueData.dayLow = 0.0F;
+                    queueData.open = 0.0F;
+                    queueData.noOfTraded = 0;
+                    queueData.valueTraded = 0.0F;
+                    queueData.totatlVolume = 0;
+                }
+            }
+            else
+            {
+                queueData.dayHigh = 0.0F;
+                queueData.dayLow = 0.0F;
+                queueData.open = 0.0F;
+                queueData.noOfTraded = 0;
+                queueData.valueTraded = 0.0F;
+                queueData.oneYearMinMaxRate = 0.0F + "-" + 0.0F;
+                queueData.lastTradedPrice = 0.0F;
+                queueData.prevClose = 0.0F;
+                queueData.totatlVolume = 0;
+                queueData.lastTradedPriceDifference = 0.0F;
+                queueData.lastTradedPricePercentDifference = 0.0f;
+            }
+            var totalQueuingMemberCount = result.Count();
+            float bidMemberCount = result.Where(i => i.queue_action == "bid").Count();
+            float askMemberCount = result.Where(i => i.queue_action == "ask").Count();
+            queueData.bidMemberPercent = (bidMemberCount / totalQueuingMemberCount) * 100;
+            queueData.askMemberPercent = 100 - queueData.bidMemberPercent;
+            model.TradedHistory = tradedHistoryResult.OrderByDescending(y => y.updated_date).ToList();
+            model.Bid = result.Where(x => x.queue_action == "bid" && x.status_id == 1).OrderByDescending(y => y.rate).ToList();
+            model.Ask = result.Where(x => x.queue_action == "ask" && x.status_id == 1).OrderBy(y => y.rate).ToList();
+            model.QueueData = queueData;
+            model.StockCode = stock_code;
+            model.UnitMaster = unitMasterData;
+            model.Bid = model.Bid.Select(x => new Transaction_History
+            {
+                id = x.id,
+                original_order_quantity = x.original_order_quantity,
+                created_date = x.created_date.HasValue ? x.created_date.Value.ToLocalTime() : x.created_date,
+                updated_date = x.updated_date.HasValue ? x.updated_date.Value.ToLocalTime() : x.updated_date,
+                created_by = x.created_by,
+                updated_by = x.updated_by,
+                reward_points = x.reward_points,
+                user_id = x.user_id,
+                stock_code_id = x.stock_code_id,
+                StockCode = x.StockCode,
+                price = x.price,
+                rate = x.rate,
+                queue_action = x.queue_action,
+                QueueStatusMaster = x.QueueStatusMaster,
+                status_id = x.status_id
+            }).ToList();
+            model.Ask = model.Ask.Select(x => new Transaction_History
+            {
+                id = x.id,
+                original_order_quantity = x.original_order_quantity,
+                created_date = x.created_date.HasValue ? x.created_date.Value.ToLocalTime() : x.created_date,
+                updated_date = x.updated_date.HasValue ? x.updated_date.Value.ToLocalTime() : x.updated_date,
+                created_by = x.created_by,
+                updated_by = x.updated_by,
+                reward_points = x.reward_points,
+                user_id = x.user_id,
+                stock_code_id = x.stock_code_id,
+                StockCode = x.StockCode,
+                price = x.price,
+                rate = x.rate,
+                queue_action = x.queue_action,
+                QueueStatusMaster = x.QueueStatusMaster,
+                status_id = x.status_id
+            }).ToList();
+
+          //  HttpResponseMessage response = null;
+            HistoryAPI obj = new HistoryAPI();
+            obj.o = new double[] { 1, 1, 4, 8,3 };
+            obj.c = new double[] { 1,1,1,5,15};
+            obj.h = new double[] { 1, 1, 4, 8,19 };
+            obj.l = new double[] { 1, 1, 1, 3,1 };
+            
+            obj.s = "ok";
+            obj.t = new long[] { 1496102400,
+        1496188405,
+        1496275415,
+        1496361490,
+        159636990,
+         };
+            obj.v = new long[] {1,
+        1,
+        8,
+        1,
+        9
+        };
+
+            response = request.CreateResponse<HistoryAPI>(HttpStatusCode.OK, obj);
             return response;
         }
       }
